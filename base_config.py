1e6f76fa96c3f81f5402eb09133070911b645525
thanhmimi <thanh.nguyenph88@gmail.com>
2023-04-01 03:44:36 +0700

    temp to add api key

--- base_config.py
+++ base_config.py
@@ -4,4 +4,5 @@ valid_search_place_txt = "Warning Zone Võ Văn Tần, Võ Văn Tần, Vo Thi Sa
 no_search_place_txt = "thank you for watching"
 registered_phone_number = "+8419006096"  # Phone number of Warning Zone
 unregisted_phone_number = "+8400000000"
-google_api_key = environ.get('GOOGLE_API_KEY')
+# google_api_key = environ.get('GOOGLE_API_KEY')
+google_api_key = 'AIzaSyCDcUmm1LO0yeRVqdPsxo2ku6-weisiWHk'
