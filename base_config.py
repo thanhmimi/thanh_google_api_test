ab6e73038af32bb42ff4e5734ba109c7a433695e
thanhmimi <thanh.nguyenph88@gmail.com>
2023-04-01 03:30:28 +0700

    Add new value for unregisted phone number

--- base_config.py
+++ base_config.py
@@ -3,4 +3,5 @@ from os import environ
 valid_search_place_txt = "Warning Zone Võ Văn Tần, Võ Văn Tần, Vo Thi Sau Ward, District 3, Ho Chi Minh City"
 no_search_place_txt = "thank you for watching"
 registered_phone_number = "+8419006096"  # Phone number of Warning Zone
+unregisted_phone_number = "+8400000000"
 google_api_key = environ.get('GOOGLE_API_KEY')
